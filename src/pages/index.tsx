cecaa14ed0f148c45640ecf0b51b59bddad7dd5b
Lamenkazu <333.erick@gmail.com>
2024-06-13 14:05:07 -0300

    ✨ Criando Carrossell com Keen Slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,58 @@
-import { styled } from '@/styles'
+import 'keen-slider/keen-slider.min.css'
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
+import { useKeenSlider } from 'keen-slider/react'
+import Image from 'next/image'
 
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  },
-})
+import camiseta1 from '@/assets/1.png'
+import camiseta2 from '@/assets/2.png'
+import camiseta3 from '@/assets/3.png'
+import { Container, Product } from '@/styles/pages/home'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <Container ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </Container>
   )
 }

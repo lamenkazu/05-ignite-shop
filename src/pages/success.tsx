fe8317a9c51379a508d173c8aad48b4ec1de2023
Lamenkazu <333.erick@gmail.com>
2024-06-17 09:40:36 -0300

    ✨ Estrutura visual página de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,22 @@
+import Link from 'next/link'
 import React from 'react'
 
-const Success = () => {
-  return <div>Success</div>
-}
+import { Container, ImageContainer } from '@/styles/pages/success'
 
-export default Success
+export default function Success() {
+  return (
+    <Container>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Erick Etiene</strong>, sua{' '}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+
+      <Link href={'/'}>Voltar ao catalogo</Link>
+    </Container>
+  )
+}

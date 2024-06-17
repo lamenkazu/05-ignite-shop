bcdca2dc85d17d8b2ba3dd1f24584ab12e9a8a7b
Lamenkazu <333.erick@gmail.com>
2024-06-17 08:19:45 -0300

    ✨ Carregando dados do produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,69 @@
-import { useRouter } from 'next/router'
-import React from 'react'
+import { GetStaticProps } from 'next'
+import Image from 'next/image'
+import Stripe from 'stripe'
 
-const Product = () => {
-  const { query } = useRouter()
+import { stripe } from '@/lib/stripe'
+import {
+  Container,
+  ImageContainer,
+  ProductDetails,
+} from '@/styles/pages/product'
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+  }
 }
 
-export default Product
+export default function Product({ product }: ProductProps) {
+  return (
+    <Container>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+
+        <p>{product.description}</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </Container>
+  )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params!.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  })
+
+  const productPrice = product.default_price as Stripe.Price
+
+  const price = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(productPrice.unit_amount! / 100)
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price,
+        description: product.description,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1 hora
+  }
+}
